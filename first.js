ddc971727c91d5f3e6553b99ac36aca9fffd912b
Vikas Gupta <guptavikash46@gmail.com>
2019-02-19 23:51:26 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*let num1 = 54,
+    num2 = 23;
+alert("The sum is : "+ Number("54"));
+*/
+
+/*let age = prompt("Your age is :", 100);
+alert(`So, your age is ${age}, hahaa!`);
+*/
+let num = prompt("Enter the number :",10);
+let func = num => num**2;
+console.log(func(num));
