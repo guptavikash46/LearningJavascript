530b7d40825027ea6fe68d403edda291d3336e6b
Vikas Gupta <guptavikash46@gmail.com>
2019-02-20 00:20:31 +0530

    BDD with Mocha & chai

--- first.js
+++ first.js
@@ -8,6 +8,13 @@ alert("The sum is : "+ Number("54"));
 /*let age = prompt("Your age is :", 100);
 alert(`So, your age is ${age}, hahaa!`);
 */
-let num = prompt("Enter the number :",10);
+/*let num = prompt("Enter the number :",10);
 let func = num => num**2;
 console.log(func(num));
+*/
+describe("pow", function () {
+
+    it('it raises x to power n', function () {
+        assert.equal(pow(2,3), 8);
+    });
+} );
