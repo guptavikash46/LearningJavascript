bb1593a7633fad8b9b71b7bd101fa360d8463829
Vikas Gupta <guptavikash46@gmail.com>
2019-03-29 19:30:25 +0530

    JS Arrays, Objects & more

--- JS/ObjectsAndThis.js
+++ JS/ObjectsAndThis.js
@@ -10,7 +10,22 @@ let user = {
         return this.age;
     }
 };
-
+/*
 alert(user);
 alert(+user);
 alert(user + 500);
+
+ */
+
+
+//Deleting elements from an array.
+let arr = ['Vikas',
+            'Jana',
+            'Germany'];
+let value = prompt('Enter the value to delete:');
+let deletedValue = arr.splice(arr.indexOf(value), 1);
+alert(`The deleted value is: ${deletedValue}`);
+
+for (let x of arr){
+    alert(x);
+}
